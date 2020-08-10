b5f11847f20ae380c3b3d1c6d9f837dbccb9c5b9
Sofiane-ai <65662407+Sofiane-ai@users.noreply.github.com>
2020-08-10 17:16:25 +0200

    Update JavaExemple.java

--- JavaExemple.java
+++ JavaExemple.java
@@ -2,8 +2,13 @@ public class JavaExemple{
 public static void main(String[] args) {
 		
 		System.out.println("test");
+	System.out.println(getValue());
+	System.out.println(getValue("Sofiane"));
 	}
 	public static String getValue(){
 		return "test";
+		}
+	public static String getValue(String name){
+		return name +"test";
 		}
 }
